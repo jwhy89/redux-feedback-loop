6a423f97907b184f261b6e5fd0faa4b5888961f6
Jarvis Yang <jarvisgyang@gmail.com>
2019-04-05 16:12:59 -0500

    set up component folders and files; moved header to its own component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+
 import './App.css';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import FeelingOne from '../FeelingOne/FeelingOne';
+import UnderstandingTwo from '../UnderstandingTwo/UnderstandingTwo';
+import SupportedThree from '../SupportedThree/SupportedThree';
+import CommentsFour from '../CommentsFour/CommentsFour';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Feedback!</h1>
-          <h4><i>Don't forget it!</i></h4>
-        </header>
+        <Header />
         <br/>
       </div>
     );
