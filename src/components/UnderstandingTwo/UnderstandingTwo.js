aacddb4c46fe6b2724c123d771e150f37f51be24
Jarvis Yang <jarvisgyang@gmail.com>
2019-04-05 19:35:01 -0500

    removed hash router from UnderstandingTwo

--- src/components/UnderstandingTwo/UnderstandingTwo.js
+++ src/components/UnderstandingTwo/UnderstandingTwo.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { HashRouter as Router } from 'react-router-dom';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 
@@ -43,16 +42,14 @@ class UnderstandingTwo extends Component {
 
     render() {
         return (
-            <Router>
-                <div>
-                    <Header />
-                    <label>How well are you understanding the content?</label>
-                    <input onChange={this.handleChangeFor}
-                        type="number" name="understanding" min="0" max="5"></input>
-                    <button onClick={this.handleClick}>Next</button>
-                    <Footer />
-                </div>
-            </Router>
+            <div>
+                <Header />
+                <label>How well are you understanding the content?</label>
+                <input onChange={this.handleChangeFor}
+                    type="number" name="understanding" min="0" max="5"></input>
+                <button onClick={this.handleClick}>Next</button>
+                <Footer />
+            </div>
         );
     }
 }
